780aa3e56d284ac42c314bfbf5d82820d85aa930
Shane <jc.delatorre@academy.opswerks.com>
2026-03-31 00:27:44 +0800

    chore: initial model migration

--- cs2_match_tracker/models.py
+++ cs2_match_tracker/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
+class User(AbstractUser):
+    class RANK_CHOICES(models.TextChoices):
+        UNRANKED = 'Unranked'
+        SILVER = 'Below 5000'
+        LIGHT_BLUE = '5000-9999'
+        DARK_BLUE = '10000-14999'
+        LIGHT_PURPLE = '15000-19999'
+        DARK_PURPLE = '20000-24999'
+        RED = '25000-29999'
+        GOLD = '30000+'
+
+    rank = models.CharField(max_length=30, choices=RANK_CHOICES.choices, default=RANK_CHOICES.UNRANKED)
+    
+class Map(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class Match(models.Model):
+    class RESULT_CHOICES(models.TextChoices):
+        WIN = 'Win'
+        LOSS = 'Loss'
+        DRAW = 'Draw'
+
+    map = models.ForeignKey(Map, on_delete=models.CASCADE, related_name='map_matches')
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='matches')
+    date_played = models.DateTimeField(auto_now_add=True)
+    result = models.CharField(max_length=10, choices=RESULT_CHOICES.choices)
+    team_score = models.IntegerField()
+    opponent_score = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.created_by.username} - {self.map.name} on {self.date_played.strftime('%Y-%m-%d %H:%M:%S')}"
+
+
+class UserMatchStat(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='match_stats')
+    match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='user_stats')
+    kills = models.IntegerField(default=0)
+    deaths = models.IntegerField(default=0)
+    assists = models.IntegerField(default=0)
+    headshots = models.IntegerField(default=0)
+    mvp_rounds = models.IntegerField(default=0)
+    damage_dealt = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ('user', 'match')
+    
+class Weapon(models.Model):
+    class WeaponType(models.TextChoices):
+        RIFLE = 'Rifle'
+        SMG = 'SMG'
+        SHOTGUN = 'Shotgun'
+        SNIPER = 'Sniper'
+        MELEE = 'Melee'
+        PISTOL = 'Pistol'
+        MACHINE_GUN = 'Machine Gun'
+
+    name = models.CharField(max_length=64)
+    weapon_type = models.CharField(max_length=20, choices=WeaponType.choices)
+
+    def __str__(self):
+        return f"{self.name} ({self.weapon_type})"
+    
+class WeaponStat(models.Model):
+    stat = models.ForeignKey(UserMatchStat, on_delete=models.CASCADE, related_name='weapon_stats')
+    weapon = models.ForeignKey(Weapon, on_delete=models.CASCADE, related_name='weapon_stats')
+    kills = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ('stat', 'weapon')
+
+    def __str__(self):
+        return f"{self.stat.user.username} - {self.weapon.name}: {self.kills} kills"
