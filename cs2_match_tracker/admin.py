352f1cb682a15ad5521501f1c1cfdadb3359fec1
Shane <jc.delatorre@academy.opswerks.com>
2026-03-31 00:28:35 +0800

    chore: register newly created models

--- cs2_match_tracker/admin.py
+++ cs2_match_tracker/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import User, Map, Match, UserMatchStat, Weapon, WeaponStat
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Map)
+admin.site.register(Match)
+admin.site.register(UserMatchStat)
+admin.site.register(Weapon)
+admin.site.register(WeaponStat)
