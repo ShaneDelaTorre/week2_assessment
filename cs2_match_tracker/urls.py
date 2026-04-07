ea302c3e77bc4f142c8b91aa37449ec5d5c9eaae
Shane <jc.delatorre@academy.opswerks.com>
2026-04-07 17:37:05 +0800

    feat: finalize endpoints

---
+++ cs2_match_tracker/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from rest_framework.routers import DefaultRouter
+
+urlpatterns = [
+    path('auth/register/', views.RegisterUserAPIView.as_view(), name='register'),
+]
+
+router = DefaultRouter()
+router.register('users', views.UserViewSet, basename='user')
+router.register('user-stats-summary', views.UserStatsSummaryViewSet, basename='user-stats-summary')
+router.register('user-matches', views.UserMatchViewSet, basename='match')
+router.register('match-stats', views.UserMatchStatViewSet, basename='match-stat')
+router.register('weapon-stats', views.WeaponStatViewSet, basename='weapon-stat')
+router.register('maps', views.MapViewSet, basename='map')
+router.register('weapons', views.WeaponViewSet, basename='weapon')
+urlpatterns += router.urls
--- cs2_match_tracker/views.py
+++ cs2_match_tracker/views.py
@@ -1,3 +1,129 @@
-from django.shortcuts import render
+from .models import Map, Weapon, User, Match, WeaponStat, UserMatchStat
+from .serializers import (
+    MapSerializer, UserRegisterSerializer, WeaponSerializer, UserSerializer, 
+    UserStatsSummarySerializer, MatchSerializer, MatchSerializerDetail,
+    UserMatchStatCreateSerializer, UserMatchStatDetailSerializer, 
+    WeaponStatCreateSerializer, WeaponStatViewSerializer
+)
+from django.db import connection
+from pprint import pprint
+from django.db.models import Prefetch
+from rest_framework import generics
+from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from .permissions import IsAuthenticatedAndOwner, IsAuthenticatedOrReadOnly
+from .filters import MatchFilter
+from rest_framework.pagination import PageNumberPagination
+class RegisterUserAPIView(generics.CreateAPIView):
+    serializer_class = UserRegisterSerializer
+    permission_classes = [AllowAny]
 
-# Create your views here.
+class UserViewSet(
+    mixins.ListModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticatedAndOwner]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return User.objects.all()
+        return User.objects.filter(id=user.id)
+
+    @action(detail=False, methods=['get', 'put', 'patch', 'delete'], url_path='me')
+    def me(self, request):
+        user = request.user
+        if request.method == 'GET':
+            serializer = self.get_serializer(user)
+            return Response(serializer.data)
+
+        if request.method in ['PUT', 'PATCH']:
+            serializer = self.get_serializer(user, data=request.data, partial=(request.method == 'PATCH'))
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+
+        if request.method == 'DELETE':
+            user.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+class UserStatsSummaryViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = UserStatsSummarySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return User.objects.prefetch_related(
+            Prefetch('matches', queryset=Match.objects.select_related('map_played')),
+            'match_stats__weapon_stats__weapon'
+        )
+
+class UserMatchViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    filterset_class = MatchFilter
+
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Match.objects.select_related('map_played').prefetch_related(
+            'user_stats__weapon_stats__weapon'
+        )
+        if user.is_staff:
+            return queryset
+        return queryset.filter(created_by=user)
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return MatchSerializerDetail
+        return MatchSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+        return serializer.data
+
+class UserMatchStatViewSet(viewsets.ModelViewSet):
+    queryset = UserMatchStat.objects.select_related('user', 'match').prefetch_related('weapon_stats__weapon')
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return UserMatchStatDetailSerializer
+        return UserMatchStatCreateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return self.queryset
+        return self.queryset.filter(user=user)
+
+class WeaponStatViewSet(viewsets.ModelViewSet):
+    queryset = WeaponStat.objects.select_related('stat__user', 'stat__match', 'weapon')
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return WeaponStatCreateSerializer
+        return WeaponStatViewSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return self.queryset
+        return self.queryset.filter(stat__user=user)
+    
+class MapViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Map.objects.all()
+    serializer_class = MapSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+class WeaponViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Weapon.objects.all()
+    serializer_class = WeaponSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
