c30c869371baeb30b7e50a8ae531b869cbc4b530
Shane <jc.delatorre@academy.opswerks.com>
2026-04-09 16:55:29 +0800

    refractor: update permission for best use case

--- cs2_match_tracker/permissions.py
+++ cs2_match_tracker/permissions.py
@@ -1,18 +1,16 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsAuthenticatedAndOwner(BasePermission):
+class IsOwnerOrAdmin(BasePermission):
     def has_permission(self, request, view):
-        if view.action == "list":
-            return request.user.is_staff
-        return request.user.is_authenticated
+        return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return obj == request.user or request.user.is_staff
-
-
-class IsAuthenticatedOrReadOnly(BasePermission):
+        return obj.user == request.user or request.user.is_staff
+    
+class IsMatchOwnerOrAdmin(BasePermission):
     def has_permission(self, request, view):
-        if request.method in ["GET", "HEAD", "OPTIONS"]:
-            return True
-        return request.user.is_authenticated
+        return request.user and request.user.is_authenticated
+    
+    def has_object_permission(self, request, view, obj):
+        return obj.created_by == request.user or request.user.is_staff
